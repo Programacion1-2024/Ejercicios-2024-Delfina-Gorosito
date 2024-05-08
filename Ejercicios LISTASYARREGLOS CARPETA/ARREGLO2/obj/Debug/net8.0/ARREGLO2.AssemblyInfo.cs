0fdbb4aae914c21c0cfdb63f30b317efb397a32a
DelfinaGorosito <166226571+DelfinaGorosito@users.noreply.github.com>
2024-05-08 09:45:34 -0300

    .

--- Ejercicios LISTASYARREGLOS CARPETA/ARREGLO2/obj/Debug/net8.0/ARREGLO2.AssemblyInfo.cs
+++ Ejercicios LISTASYARREGLOS CARPETA/ARREGLO2/obj/Debug/net8.0/ARREGLO2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ARREGLO2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a58ccceb5fad4819a4f4d64d81f5601cee8b05f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ARREGLO2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ARREGLO2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
